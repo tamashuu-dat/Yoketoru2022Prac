93c8404e01657a770e5278910bdebeee70c051f7
A601-13 <tnk.dat.class@gmail.com>
2022-10-12 10:30:28 +0900

    ゲームループ出来上がり

--- Assets/Yoketoru/Scripts/CallChangeScene.cs
+++ Assets/Yoketoru/Scripts/CallChangeScene.cs
@@ -7,15 +7,22 @@ public class CallChangeScene : MonoBehaviour
 
     ToNextScene toNextScene;
 
+    static float IgnoreKeySeconds => 0.5f;
+    float ignoreTime;
+
     // Start is called before the first frame update
     void Start()
     {
         toNextScene = GetComponent<ToNextScene>();
+        ignoreTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ignoreTime += Time.deltaTime;
+        if (ignoreTime < IgnoreKeySeconds) return;
+
         if (Input.GetButtonDown("Next"))
         {
             toNextScene.ChangeScene();
