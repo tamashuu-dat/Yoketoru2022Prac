76b3634e46ca27896131360a7c551102b7a3ce46
A601-13 <tnk.dat.class@gmail.com>
2022-10-20 16:10:27 +0900

    スコア実装wip

--- Assets/Yoketoru/Scripts/GameManager.cs
+++ Assets/Yoketoru/Scripts/GameManager.cs
@@ -4,9 +4,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI scoreText = default;
+
+    static int score;
+    static float time;
+    static float StartTime => 10;//複数シーンで使いたいものは static を使う
+
+    private void Awake()
+    {
+        score = 0;
+        time = StartTime;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +39,19 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene("Clear", LoadSceneMode.Additive);
         }
+        else if(Input.GetKeyDown(KeyCode.P))
+        {
+            score += 123;
+            UpdateScoreText();
+        }
 #endif
+    }
+
+    void UpdateScoreText()
+    {
+        if(scoreText!=null)
+        {
+            scoreText.text = $"{score:00000}";
+        }
     }
 }
